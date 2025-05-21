83c7f7f368ad3d83f600e0e5f78fa72309160493
hngmo.kim <112348881+seocheonf@users.noreply.github.com>
2025-05-21 14:54:49 +0900

    Initial Commiet [ 2025-05-21 ]
    About gitignore

---
+++ Source/DuoShooting/DuoShooting.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class DuoShooting : ModuleRules
+{
+	public DuoShooting(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+	}
+}
---
+++ Source/DuoShooting/DuoShootingGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DuoShootingGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ADuoShootingGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ADuoShootingGameMode();
+};
+
+
+

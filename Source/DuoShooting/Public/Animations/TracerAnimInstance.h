03e869b456fe497cb7b03acc0b1074e59fc12774
Squidette <qfkrwldnjs@naver.com>
2025-06-10 17:01:06 +0900

    0601 create tracer abp

---
+++ Source/DuoShooting/Public/Animations/TracerAnimInstance.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "TracerAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DUOSHOOTING_API UTracerAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+};

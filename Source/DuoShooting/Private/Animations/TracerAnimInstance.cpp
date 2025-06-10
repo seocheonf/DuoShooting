4db67d54d415aeeb3555ed9a311e1f86fd5b428c
Squidette <qfkrwldnjs@naver.com>
2025-06-10 18:46:36 +0900

    0610 tracer abp walk, pitch

--- Source/DuoShooting/Private/Animations/TracerAnimInstance.cpp
+++ Source/DuoShooting/Private/Animations/TracerAnimInstance.cpp
@@ -2,3 +2,34 @@
 
 
 #include "Animations/TracerAnimInstance.h"
+
+#include "Player/TracerHero.h"
+
+void UTracerAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+	tracer = Cast<ATracerHero>(TryGetPawnOwner());
+}
+
+void UTracerAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	if (tracer)
+	{
+		FVector vel = tracer->GetVelocity();
+
+		// 캐릭터의 속도를 가져와서 부드럽게 스피드와 방향 계산
+		float rawSpeed = FMath::GetMappedRangeValueClamped(FVector2D(-1.0f, 1.0f), FVector2D(0.0f, 100.0f),
+		                                          FVector::DotProduct(vel, tracer->GetActorForwardVector()));
+		
+		float rawDir = FMath::GetMappedRangeValueClamped(FVector2D(-1.0f, 1.0f), FVector2D(0.0f, 100.0f),
+										  FVector::DotProduct(vel, tracer->GetActorRightVector()));
+
+		Speed = FMath::FInterpTo(Speed, rawSpeed, DeltaSeconds, LegsBlendSpaceInterpSpeed);
+		Direction = FMath::FInterpTo(Direction, rawDir, DeltaSeconds, LegsBlendSpaceInterpSpeed);
+
+		PitchAngle = -tracer->GetBaseAimRotation().GetNormalized().Pitch;
+		PitchAngle = FMath::Clamp(PitchAngle, -60.0f, 60.0f);
+	}
+}
--- Source/DuoShooting/Public/Animations/TracerAnimInstance.h
+++ Source/DuoShooting/Public/Animations/TracerAnimInstance.h
@@ -13,4 +13,25 @@ UCLASS()
 class DUOSHOOTING_API UTracerAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
+
+private:
+	float LegsBlendSpaceInterpSpeed = 15.0f;
+	
+protected:
+	virtual void NativeInitializeAnimation() override;
+
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+	
+	UPROPERTY()
+	class ATracerHero* tracer;
+	
+public:
+	UPROPERTY(BlueprintReadWrite)
+	float Direction;
+
+	UPROPERTY(BlueprintReadWrite)
+	float Speed;
+
+	UPROPERTY(BlueprintReadWrite)
+	float PitchAngle;
 };
